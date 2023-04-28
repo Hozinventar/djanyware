c0a88a9bf594975eac94e0d527bf323371602d13
maguta <erehinskiy@x>
2023-04-28 21:16:05 +0300

    python manage.py collectstatic

--- mysite/settings.py
+++ mysite/settings.py
@@ -15,7 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+print(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -130,10 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATIC_ROOT = ""
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATIC_ROOT = ""
 STATIC_URL = '/static/'
-STATICFILES_DIRS = ('static',)
+# STATICFILES_DIRS = ('static',)
 # STATICFILES_DIRS = (STATIC_ROOT,)
 # STATICFILES_FINDERS = (
 #     'django.contrib.staticfiles.finders.FileSystemFinder',
