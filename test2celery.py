fde2add78eae8c2b215baf001f85c6fc3af6f36c
maguta <erehinskiy@x>
2023-04-29 23:37:18 +0300

    celery

--- test2celery.py
+++ test2celery.py
@@ -5,10 +5,13 @@ django.setup()
 
 # таска без ретерна
 from mysite.celery import debug_task
-debug_task.delay()
+# debug_task.delay()
 
 # таска с ретерном
 from woman.tasks import bar
-bar.delay()
+# bar.delay()
 
-print('d')
+from django_celery_results.models import TaskResult
+r = TaskResult.objects.last()
+
+print(r.__dict__)
